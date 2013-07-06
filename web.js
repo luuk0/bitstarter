90802f770a416ef69e443e31818c1e59afcc6eda
Luk <luk@luk-vb.(none)>
2013-07-06 08:40:41 +0200

    Update web.js - read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.write(fs.readFileSync("index.html").toString());
+  response.end();
 });
 
 var port = process.env.PORT || 5000;
